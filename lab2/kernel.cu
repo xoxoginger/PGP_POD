ddd9924ea700bd681af784d5945a1f91b9899896
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-04-14 03:57:08 +0300

    защита:done

--- lab2/kernel.cu
+++ lab2/kernel.cu
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+#define CSC(call)  													\
+do {																\
+	cudaError_t res = call;											\
+	if (res != cudaSuccess) {										\
+		fprintf(stderr, "ERROR in %s:%d. Message: %s\n",			\
+				__FILE__, __LINE__, cudaGetErrorString(res));		\
+		exit(0);													\
+	}																\
+} while(0)
+
 // текстурная ссылка <тип элементов, размерность, режим нормализации>
 texture<uchar4, 2, cudaReadModeElementType> tex;
 
@@ -16,29 +26,54 @@ __global__ void bilinear_interp_kernel(uchar4* out, int w, int h, int wn, int hn
 	int idy = blockDim.y * blockIdx.y + threadIdx.y;
 	int offsetx = blockDim.x * gridDim.x;
 	int offsety = blockDim.y * gridDim.y;
-	int x, y, x_n, y_n;
-	double x2, y2;
+	int x, y;
+	int x1, y1;
 	uchar4 p1, p2, p3, p4;
-	char r, g, b;
-	
+	double r, g, b;
+	double x2, y2;
 	for (y = idy; y < hn; y += offsety)
 		for (x = idx; x < wn; x += offsetx)
 		{
-			y_n = max(min(y, h), 1);
-			x_n = max(min(x, w), 1);
-			//x_n = (x + 0.5) * w / wn - 0.5;
-			//y_n = (y + 0.5) * h / hn - 0.5;
+			double h_d = (double)h / hn;
+			double w_d = (double)w / wn;
 
-			p1 = tex2D(tex, x_n, y_n); //01
-			p2 = tex2D(tex, x_n + 1, y_n); //11
-			p3 = tex2D(tex, x_n, y_n + 1); // 00
-			p4 = tex2D(tex, x_n + 1, y_n + 1); //10
+			double x_n = (x + 0.5) * w_d - 0.5;
+			double y_n = (y + 0.5) * h_d - 0.5;
 
+			x1 = x_n; //x_r
+			x2 = x_n - x1;//ceil(x_n);
+			if (x2 < 0.0) //////////
+			{
+				x2++;
+				x1--;
+			}
+			y1 = y_n;
+			y2 = y_n - y1; //ceil(y_n);
+			if (y2 < 0.0) /////////
+			{ 
+				y2++;
+				y1--;
+			}
+				
+			//printf("%f %f %f %f \n", x1, x2, y1, y2); +
 			
-			r = (p1.x * (x_n + 1 - x) * (y_n + 1 - y) + p2.x * (x - x_n) * (y_n + 1 - y) + p3.x * (x_n + 1 - x) * (y - y_n) + p4.x * (x - x_n) * (y - y_n)); // ((x_n+1 - x_n) * (y_n +1 - y_n));
-			g = (p1.y * (x_n + 1 - x) * (y_n + 1 - y) + p2.y * (x - x_n) * (y_n + 1 - y) + p3.y * (x_n + 1 - x) * (y - y_n) + p4.y * (x - x_n) * (y - y_n)); // ((x_n + 1 - x_n) * (y_n + 1 - y_n));
-			b = (p1.z * (x_n + 1 - x) * (y_n + 1 - y) + p2.z * (x - x_n) * (y_n + 1 - y) + p3.z * (x_n + 1 - x) * (y - y_n) + p4.z * (x - x_n) * (y - y_n)); // ((x_n + 1 - x_n) * (y_n + 1 - y_n));
+		    p1 = tex2D(tex, x1, y1);
+			p2 = tex2D(tex, x1 + 1, y1); 
+			p3 = tex2D(tex, x1, y1 + 1); 
+			p4 = tex2D(tex, x1 + 1, y1 + 1); 
+
+			//r = (p1.x * (x2 - x_n) * (y2 - y_n) + p2.x * (x_n - x1) * (y2 - y_n) + p3.x * (x2 - x_n) * (y_n - y1) + p4.x * (x_n - x1) * (y_n - y1)) / ((x2 - x1) * (y2 - y1));
+			//g = (p1.y * (x2 - x_n) * (y2 - y_n) + p2.y * (x_n - x1) * (y2 - y_n) + p3.y * (x2 - x_n) * (y_n - y1) + p4.y * (x_n - x1) * (y_n - y1)) / ((x2 - x1) * (y2 - y1));
+			//b = (p1.z * (x2 - x_n) * (y2 - y_n) + p2.z * (x_n - x1) * (y2 - y_n) + p3.z * (x2 - x_n) * (y_n - y1) + p4.z * (x_n - x1) * (y_n - y1)) / ((x2 - x1) * (y2 - y1));
+
+			r = p1.x * (1.0 - x2) * (1.0 - y2) + p2.x * x2 * (1.0 - y2) + p3.x * (1.0 - x2) * y2 + p4.x * x2 * y2;
+			g = p1.y * (1.0 - x2) * (1.0 - y2) + p2.y * x2 * (1.0 - y2) + p3.y * (1.0 - x2) * y2 + p4.y * x2 * y2;
+			b = p1.z * (1.0 - x2) * (1.0 - y2) + p2.z * x2 * (1.0 - y2) + p3.z * (1.0 - x2) * y2 + p4.z * x2 * y2;
 			
+			//if (r > 255)
+			//{
+				//printf("SHIT\n");
+			//}
 
 			out[y * wn + x] = make_uchar4(r, g, b, p1.w);
 		}
@@ -52,7 +87,6 @@ int main() {
 	std::cin >> inFile >> outFile >> wn >> hn;
 
 	FILE* fp = fopen(inFile.c_str(), "rb");
-
 	//FILE* fp = fopen("in.data", "rb");
 	fread(&w, sizeof(int), 1, fp);
 	fread(&h, sizeof(int), 1, fp);
@@ -61,28 +95,14 @@ int main() {
 	fread(data, sizeof(uchar4), w * h, fp);
 	fclose(fp);
 
-	cudaError_t cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?:)";
-		//memfree
-		return 0;
-	}
+	CSC(cudaSetDevice(0));
+
 	// Подготовка данных для текстуры
 	cudaArray* arr;
 	cudaChannelFormatDesc ch = cudaCreateChannelDesc<uchar4>();
-	cudaStatus = cudaMallocArray(&arr, &ch, w, h);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaMalloc failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
+	CSC(cudaMallocArray(&arr, &ch, w, h));
 
-	cudaStatus = cudaMemcpyToArray(arr, 0, 0, data, sizeof(uchar4) * w * h, cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaMemcpy failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
+	CSC(cudaMemcpyToArray(arr, 0, 0, data, sizeof(uchar4) * w * h, cudaMemcpyHostToDevice));
 
 	// Подготовка текстурной ссылки, настройка интерфейса работы с данными
 	tex.addressMode[0] = cudaAddressModeClamp;	// Политика обработки выхода за границы по каждому измерению
@@ -92,54 +112,22 @@ int main() {
 	tex.normalized = false;						// Режим нормализации координат: без нормализации
 
 	// Связываем интерфейс с данными
-	cudaStatus = cudaBindTextureToArray(tex, arr, ch);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaBindTexture failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
+	CSC(cudaBindTextureToArray(tex, arr, ch));
 
 	uchar4* out;
 
-	cudaStatus = cudaMalloc(&out, sizeof(uchar4) * wn * hn);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaMalloc failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
-
-	//bool f;
+	CSC(cudaMalloc(&out, sizeof(uchar4) * wn * hn));
 
 	bilinear_interp_kernel << <dim3(16, 16), dim3(16, 16) >> > (out, w, h, wn, hn);
 
-	cudaMemcpy(data_, out, sizeof(uchar4) * wn * hn, cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaMemcpy failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
+	CSC(cudaMemcpy(data_, out, sizeof(uchar4) * wn * hn, cudaMemcpyDeviceToHost));
 
 	// Отвязываем данные от текстурной ссылки
-	cudaStatus = cudaUnbindTexture(tex);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaUnBindTexture failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
+	CSC(cudaUnbindTexture(tex));
 
-	cudaStatus = cudaFreeArray(arr);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaFreeArray failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 1;
-	}
+	CSC(cudaFreeArray(arr));
 
-	cudaStatus = cudaFree(out);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaFree failed!";
-		//MemFree(va, vb, vc, v1, v2, v3);
-		return 0;
-	}
+	CSC(cudaFree(out));
 
 	fp = fopen(outFile.c_str(), "wb");
 	fwrite(&wn, sizeof(int), 1, fp);
